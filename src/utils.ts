b52bf8533010ef020acc43521f94112b28a5f8df
danijelTxFusion <danijel@txfusion.io>
2024-01-21 19:17:40 +0100

    refactor(utils): move paymaster ABI interface to `paymaster-utils.ts` file

--- src/utils.ts
+++ src/utils.ts
@@ -24,7 +24,6 @@ export const IERC1271 = new ethers.Interface(require("../abi/IERC1271.json"));
 export const L1_BRIDGE_ABI = new ethers.Interface(require("../abi/IL1Bridge.json"));
 export const L2_BRIDGE_ABI = new ethers.Interface(require("../abi/IL2Bridge.json"));
 export const NONCE_HOLDER_ABI = new ethers.Interface(require("../abi/INonceHolder.json"));
-export const PAYMASTER_FLOW_ABI = new ethers.Interface(require("../abi/IPaymasterFlow.json"));
 
 export const ETH_ADDRESS = "0x0000000000000000000000000000000000000000";
 export const BOOTLOADER_FORMAL_ADDRESS = "0x0000000000000000000000000000000000008001";
