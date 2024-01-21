56f8094c61e724d03d3c78754a882658e89ecfd1
danijelTxFusion <danijel@txfusion.io>
2024-01-21 15:22:27 +0100

    feat(utils): remove deprecated `IPaymasterFlow` ABI
    BREAKING CHANGE: remove the deprecated `utils.IPaymasterFlow` in
    favor of `utils.PAYMASTER_FLOW_ABI`.

--- src/paymaster-utils.ts
+++ src/paymaster-utils.ts
@@ -9,9 +9,6 @@ import {
     PaymasterParams,
 } from "./types";
 
-/** @deprecated This ABI is here for backward compatibility - please use utils.PAYMASTER_FLOW_ABI instead */
-export const IPaymasterFlow = new ethers.Interface(require("../abi/IPaymasterFlow.json"));
-
 export function getApprovalBasedPaymasterInput(paymasterInput: ApprovalBasedPaymasterInput): BytesLike {
     return PAYMASTER_FLOW_ABI.encodeFunctionData("approvalBased", [
         paymasterInput.token,
