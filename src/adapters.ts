44d8600e18588e2278a8ede012432b2e3d825098
danijelTxFusion <danijel@txfusion.io>
2024-01-23 20:16:26 +0100

    refactor(adapter): add missing return types

--- src/adapters.ts
+++ src/adapters.ts
@@ -38,7 +38,7 @@ import {
 import {
     Address,
     BalancesMap,
-    Eip712Meta,
+    Eip712Meta, FinalizeWithdrawalParams,
     FullDepositFee,
     MessageProof,
     PriorityOpResponse,
@@ -528,7 +528,7 @@ export function AdapterL1<TBase extends Constructor<TxSender>>(Base: TBase) {
             };
         }
 
-        async finalizeWithdrawalParams(withdrawalHash: BytesLike, index: number = 0) {
+        async finalizeWithdrawalParams(withdrawalHash: BytesLike, index: number = 0): Promise<FinalizeWithdrawalParams> {
             const { log, l1BatchTxId } = await this._getWithdrawalLog(withdrawalHash, index);
             const { l2ToL1LogIndex } = await this._getWithdrawalL2ToL1Log(withdrawalHash, index);
             const sender = ethers.dataSlice(log.topics[1], 12);
@@ -596,7 +596,7 @@ export function AdapterL1<TBase extends Constructor<TxSender>>(Base: TBase) {
             );
         }
 
-        async isWithdrawalFinalized(withdrawalHash: BytesLike, index: number = 0) {
+        async isWithdrawalFinalized(withdrawalHash: BytesLike, index: number = 0): Promise<boolean> {
             const { log } = await this._getWithdrawalLog(withdrawalHash, index);
             const { l2ToL1LogIndex } = await this._getWithdrawalL2ToL1Log(withdrawalHash, index);
             const sender = ethers.dataSlice(log.topics[1], 12);
