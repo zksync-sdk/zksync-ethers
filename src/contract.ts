115c5dd1c58c3be009004819b967527bf5c59ab7
danijelTxFusion <danijel@txfusion.io>
2024-02-07 22:16:07 +0100

    docs(contracts): update docs for `ContractFactory.deploy` method

--- src/contract.ts
+++ src/contract.ts
@@ -123,28 +123,28 @@ export class ContractFactory extends ethers.ContractFactory {
      *
      * @async
      * @param {...Array<any>} args - Constructor arguments for the contract followed by optional
-     * {@link ethers.PayableOverrides|overrides}. When deploying with CREATE2 opcode slat must be present in overrides.
+     * {@link ethers.PayableOverrides|overrides}. When deploying with CREATE2 method slat must be present in overrides.
      *
      *
      * @example
-     * // Deploy with constructor arguments only using CREATE opcode
+     * // Deploy with constructor arguments only using CREATE method
      * const deployedContract = await contractFactory.deploy(arg1, arg2, ...);
      *
-     * // Deploy with constructor arguments, and factory dependencies using CREATE opcode
+     * // Deploy with constructor arguments, and factory dependencies using CREATE method
      * const deployedContractWithSaltAndDeps = await contractFactory.deploy(arg1, arg2, ..., {
      *   customData: {
      *     factoryDeps: ['0x...']
      *   }
      * });
      *
-     * // Deploy with constructor arguments and custom salt using CREATE2 opcode
+     * // Deploy with constructor arguments and custom salt using CREATE2 method
      * const deployedContractWithSalt = await contractFactory.deploy(arg1, arg2, ..., {
      *   customData: {
      *     salt: '0x...'
      *   }
      * });
      *
-     * // Deploy with constructor arguments, custom salt, and factory dependencies using CREATE2 opcode
+     * // Deploy with constructor arguments, custom salt, and factory dependencies using CREATE2 method
      * const deployedContractWithSaltAndDeps = await contractFactory.deploy(arg1, arg2, ..., {
      *   customData: {
      *     salt: '0x...',
