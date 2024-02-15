fae22333fb039bf42cfadaf5467c4ced8bf012b3
danijelTxFusion <danijel@txfusion.io>
2024-02-15 07:03:17 +0100

    refactor: fix lint errors

--- src/signer.ts
+++ src/signer.ts
@@ -1,89 +1,107 @@
-import { ethers } from "ethers";
-import { Provider } from "./provider";
-import { DEFAULT_GAS_PER_PUBDATA_LIMIT, EIP712_TX_TYPE, hashBytecode, serializeEip712 } from "./utils";
-import { Address, Signature, TransactionLike, TransactionRequest, TransactionResponse } from "./types";
-import { AdapterL1, AdapterL2 } from "./adapters";
+import {ethers} from 'ethers';
+import {Provider} from './provider';
+import {
+  DEFAULT_GAS_PER_PUBDATA_LIMIT,
+  EIP712_TX_TYPE,
+  hashBytecode,
+  serializeEip712,
+} from './utils';
+import {
+  Address,
+  Signature,
+  TransactionLike,
+  TransactionRequest,
+  TransactionResponse,
+} from './types';
+import {AdapterL1, AdapterL2} from './adapters';
 
 export const EIP712_TYPES = {
-    Transaction: [
-        { name: "txType", type: "uint256" },
-        { name: "from", type: "uint256" },
-        { name: "to", type: "uint256" },
-        { name: "gasLimit", type: "uint256" },
-        { name: "gasPerPubdataByteLimit", type: "uint256" },
-        { name: "maxFeePerGas", type: "uint256" },
-        { name: "maxPriorityFeePerGas", type: "uint256" },
-        { name: "paymaster", type: "uint256" },
-        { name: "nonce", type: "uint256" },
-        { name: "value", type: "uint256" },
-        { name: "data", type: "bytes" },
-        { name: "factoryDeps", type: "bytes32[]" },
-        { name: "paymasterInput", type: "bytes" },
-    ],
+  Transaction: [
+    {name: 'txType', type: 'uint256'},
+    {name: 'from', type: 'uint256'},
+    {name: 'to', type: 'uint256'},
+    {name: 'gasLimit', type: 'uint256'},
+    {name: 'gasPerPubdataByteLimit', type: 'uint256'},
+    {name: 'maxFeePerGas', type: 'uint256'},
+    {name: 'maxPriorityFeePerGas', type: 'uint256'},
+    {name: 'paymaster', type: 'uint256'},
+    {name: 'nonce', type: 'uint256'},
+    {name: 'value', type: 'uint256'},
+    {name: 'data', type: 'bytes'},
+    {name: 'factoryDeps', type: 'bytes32[]'},
+    {name: 'paymasterInput', type: 'bytes'},
+  ],
 };
 
 export class EIP712Signer {
-    private eip712Domain: Promise<ethers.TypedDataDomain>;
-    constructor(
-        private ethSigner: ethers.Signer,
-        chainId: number | Promise<number>,
-    ) {
-        this.eip712Domain = Promise.resolve(chainId).then((chainId) => ({
-            name: "zkSync",
-            version: "2",
-            chainId,
-        }));
-    }
+  private eip712Domain: Promise<ethers.TypedDataDomain>;
+  constructor(
+    private ethSigner: ethers.Signer,
+    chainId: number | Promise<number>
+  ) {
+    this.eip712Domain = Promise.resolve(chainId).then(chainId => ({
+      name: 'zkSync',
+      version: '2',
+      chainId,
+    }));
+  }
 
-    static getSignInput(transaction: TransactionRequest) {
-        const maxFeePerGas = transaction.maxFeePerGas || transaction.gasPrice || 0n;
-        const maxPriorityFeePerGas = transaction.maxPriorityFeePerGas || maxFeePerGas;
-        const gasPerPubdataByteLimit =
-            transaction.customData?.gasPerPubdata || DEFAULT_GAS_PER_PUBDATA_LIMIT;
-        return {
-            txType: transaction.type,
-            from: transaction.from,
-            to: transaction.to,
-            gasLimit: transaction.gasLimit || 0n,
-            gasPerPubdataByteLimit: gasPerPubdataByteLimit,
-            maxFeePerGas,
-            maxPriorityFeePerGas,
-            paymaster: transaction.customData?.paymasterParams?.paymaster || ethers.ZeroAddress,
-            nonce: transaction.nonce || 0,
-            value: transaction.value || 0n,
-            data: transaction.data || "0x",
-            factoryDeps: transaction.customData?.factoryDeps?.map((dep: any) => hashBytecode(dep)) || [],
-            paymasterInput: transaction.customData?.paymasterParams?.paymasterInput || "0x",
-        };
-    }
+  static getSignInput(transaction: TransactionRequest) {
+    const maxFeePerGas = transaction.maxFeePerGas || transaction.gasPrice || 0n;
+    const maxPriorityFeePerGas =
+      transaction.maxPriorityFeePerGas || maxFeePerGas;
+    const gasPerPubdataByteLimit =
+      transaction.customData?.gasPerPubdata || DEFAULT_GAS_PER_PUBDATA_LIMIT;
+    return {
+      txType: transaction.type,
+      from: transaction.from,
+      to: transaction.to,
+      gasLimit: transaction.gasLimit || 0n,
+      gasPerPubdataByteLimit: gasPerPubdataByteLimit,
+      maxFeePerGas,
+      maxPriorityFeePerGas,
+      paymaster:
+        transaction.customData?.paymasterParams?.paymaster ||
+        ethers.ZeroAddress,
+      nonce: transaction.nonce || 0,
+      value: transaction.value || 0n,
+      data: transaction.data || '0x',
+      factoryDeps:
+        transaction.customData?.factoryDeps?.map((dep: any) =>
+          hashBytecode(dep)
+        ) || [],
+      paymasterInput:
+        transaction.customData?.paymasterParams?.paymasterInput || '0x',
+    };
+  }
 
-    async sign(transaction: TransactionRequest): Promise<Signature> {
-        return await this.ethSigner.signTypedData(
-            await this.eip712Domain,
-            EIP712_TYPES,
-            EIP712Signer.getSignInput(transaction),
-        );
-    }
+  async sign(transaction: TransactionRequest): Promise<Signature> {
+    return await this.ethSigner.signTypedData(
+      await this.eip712Domain,
+      EIP712_TYPES,
+      EIP712Signer.getSignInput(transaction)
+    );
+  }
 
-    static getSignedDigest(transaction: TransactionRequest): ethers.BytesLike {
-        if (!transaction.chainId) {
-            throw Error("Transaction chainId isn't set!");
-        }
-        const domain = {
-            name: "zkSync",
-            version: "2",
-            chainId: transaction.chainId,
-        };
-        return ethers.TypedDataEncoder.hash(
-            domain,
-            EIP712_TYPES,
-            EIP712Signer.getSignInput(transaction),
-        );
+  static getSignedDigest(transaction: TransactionRequest): ethers.BytesLike {
+    if (!transaction.chainId) {
+      throw Error("Transaction chainId isn't set!");
     }
+    const domain = {
+      name: 'zkSync',
+      version: '2',
+      chainId: transaction.chainId,
+    };
+    return ethers.TypedDataEncoder.hash(
+      domain,
+      EIP712_TYPES,
+      EIP712Signer.getSignInput(transaction)
+    );
+  }
 
-    async getDomain(): Promise<ethers.TypedDataDomain> {
-        return await this.eip712Domain;
-    }
+  async getDomain(): Promise<ethers.TypedDataDomain> {
+    return await this.eip712Domain;
+  }
 }
 
 // This class is to be used on the frontend, with metamask injection.
@@ -94,60 +112,65 @@ export class EIP712Signer {
 // const tx = await signer.sendTransaction({ ... });
 /* c8 ignore start */
 export class Signer extends AdapterL2(ethers.JsonRpcSigner) {
-    // @ts-ignore
-    public override provider: Provider;
-    // @ts-ignore
-    public eip712: EIP712Signer;
+  public override provider!: Provider;
+  public eip712!: EIP712Signer;
 
-    override _signerL2() {
-        return this;
+  override _signerL2() {
+    return this;
+  }
+
+  override _providerL2() {
+    return this.provider;
+  }
+
+  static from(
+    signer: ethers.JsonRpcSigner & {provider: Provider},
+    chainId: number
+  ): Signer {
+    const newSigner: Signer = Object.setPrototypeOf(signer, Signer.prototype);
+    newSigner.eip712 = new EIP712Signer(newSigner, chainId);
+    return newSigner;
+  }
+
+  override async sendTransaction(
+    transaction: TransactionRequest
+  ): Promise<TransactionResponse> {
+    if (!transaction.customData && !transaction.type) {
+      // use legacy txs by default
+      transaction.type = 0;
     }
+    if (!transaction.customData && transaction.type !== EIP712_TX_TYPE) {
+      return (await super.sendTransaction(transaction)) as TransactionResponse;
+    } else {
+      const address = await this.getAddress();
+      const from = !transaction.from
+        ? address
+        : await ethers.resolveAddress(transaction.from as Address);
+      if (from.toLowerCase() !== address.toLowerCase()) {
+        throw new Error('Transaction `from` address mismatch!');
+      }
+      const tx: TransactionLike = {
+        type: transaction.type ?? EIP712_TX_TYPE,
+        value: transaction.value ?? 0,
+        data: transaction.data ?? '0x',
+        nonce: transaction.nonce ?? (await this.getNonce()),
+        gasPrice: transaction.gasPrice ?? (await this.provider.getGasPrice()),
+        gasLimit:
+          transaction.gasLimit ??
+          (await this.provider.estimateGas(transaction)),
+        chainId:
+          transaction.chainId ?? (await this.provider.getNetwork()).chainId,
+        to: await ethers.resolveAddress(transaction.to as Address),
+        customData: this._fillCustomData(transaction.customData ?? {}),
+        from,
+      };
+      tx.customData ??= {};
+      tx.customData.customSignature = await this.eip712.sign(tx);
 
-    override _providerL2() {
-        return this.provider;
+      const txBytes = serializeEip712(tx);
+      return await this.provider.broadcastTransaction(txBytes);
     }
-
-    static from(signer: ethers.JsonRpcSigner & { provider: Provider }, chainId: number): Signer {
-        const newSigner: Signer = Object.setPrototypeOf(signer, Signer.prototype);
-        newSigner.eip712 = new EIP712Signer(newSigner, chainId);
-        return newSigner;
-    }
-
-    override async sendTransaction(transaction: TransactionRequest): Promise<TransactionResponse> {
-        if (transaction.customData == null && transaction.type == null) {
-            // use legacy txs by default
-            transaction.type = 0;
-        }
-        if (transaction.customData == null && transaction.type != EIP712_TX_TYPE) {
-            return (await super.sendTransaction(transaction)) as TransactionResponse;
-        } else {
-            const address = await this.getAddress();
-            const from =
-                transaction.from == null
-                    ? address
-                    : await ethers.resolveAddress(transaction.from as Address);
-            if (from.toLowerCase() != address.toLowerCase()) {
-                throw new Error("Transaction `from` address mismatch!");
-            }
-            const tx: TransactionLike = {
-                type: transaction.type ?? EIP712_TX_TYPE,
-                value: transaction.value ?? 0,
-                data: transaction.data ?? "0x",
-                nonce: transaction.nonce ?? (await this.getNonce()),
-                gasPrice: transaction.gasPrice ?? (await this.provider.getGasPrice()),
-                gasLimit: transaction.gasLimit ?? (await this.provider.estimateGas(transaction)),
-                chainId: transaction.chainId ?? (await this.provider.getNetwork()).chainId,
-                to: await ethers.resolveAddress(transaction.to as Address),
-                customData: this._fillCustomData(transaction.customData ?? {}),
-                from,
-            };
-            // @ts-ignore
-            tx.customData.customSignature = await this.eip712.sign(tx);
-
-            const txBytes = serializeEip712(tx);
-            return await this.provider.broadcastTransaction(txBytes);
-        }
-    }
+  }
 }
 
 // This class is to be used on the frontend with metamask injection.
@@ -158,29 +181,34 @@ export class Signer extends AdapterL2(ethers.JsonRpcSigner) {
 // const signer = L1Signer.from(ethProvider.getSigner(), provider);
 // const tx = await signer.deposit({ ... });
 export class L1Signer extends AdapterL1(ethers.JsonRpcSigner) {
-    // @ts-ignore
-    public providerL2: Provider;
-    override _providerL2() {
-        return this.providerL2;
-    }
+  public providerL2!: Provider;
+  override _providerL2() {
+    return this.providerL2;
+  }
 
-    override _providerL1() {
-        return this.provider;
-    }
+  override _providerL1() {
+    return this.provider;
+  }
 
-    override _signerL1() {
-        return this;
-    }
+  override _signerL1() {
+    return this;
+  }
 
-    static from(signer: ethers.JsonRpcSigner, zksyncProvider: Provider): L1Signer {
-        const newSigner: L1Signer = Object.setPrototypeOf(signer, L1Signer.prototype);
-        newSigner.providerL2 = zksyncProvider;
-        return newSigner;
-    }
+  static from(
+    signer: ethers.JsonRpcSigner,
+    zksyncProvider: Provider
+  ): L1Signer {
+    const newSigner: L1Signer = Object.setPrototypeOf(
+      signer,
+      L1Signer.prototype
+    );
+    newSigner.providerL2 = zksyncProvider;
+    return newSigner;
+  }
 
-    connectToL2(provider: Provider): this {
-        this.providerL2 = provider;
-        return this;
-    }
+  connectToL2(provider: Provider): this {
+    this.providerL2 = provider;
+    return this;
+  }
 }
 /* c8 ignore stop */
