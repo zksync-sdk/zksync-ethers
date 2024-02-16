deb8b2013f0912745bdc7959179f4a83af16cbd5
danijelTxFusion <danijel@txfusion.io>
2024-02-16 04:10:01 +0100

    docs: document the source code

--- src/paymaster-utils.ts
+++ src/paymaster-utils.ts
@@ -8,10 +8,20 @@ import {
   PaymasterParams,
 } from './types';
 
+/**
+ * The ABI for the `IPaymasterFlow` interface, which is utilized
+ * for encoding input parameters for paymaster flows.
+ * @constant
+ */
 export const PAYMASTER_FLOW_ABI = new ethers.Interface(
   require('../abi/IPaymasterFlow.json')
 );
 
+/**
+ * Returns encoded input for an approval-based paymaster.
+ *
+ * @param paymasterInput The input data for the paymaster.
+ */
 export function getApprovalBasedPaymasterInput(
   paymasterInput: ApprovalBasedPaymasterInput
 ): BytesLike {
@@ -22,6 +32,11 @@ export function getApprovalBasedPaymasterInput(
   ]);
 }
 
+/**
+ * Returns encoded input for a general-based paymaster.
+ *
+ * @param paymasterInput The input data for the paymaster.
+ */
 export function getGeneralPaymasterInput(
   paymasterInput: GeneralPaymasterInput
 ): BytesLike {
@@ -30,6 +45,29 @@ export function getGeneralPaymasterInput(
   ]);
 }
 
+/**
+ * Returns a correctly-formed {@link PaymasterParams|paymasterParams} object for common paymaster flows.
+ *
+ * @param paymasterAddress The non-zero paymaster address.
+ * @param paymasterInput The input data for the paymaster.
+ *
+ * @example create general-based parameters
+ *
+ * const paymasterAddress = "0x0a67078A35745947A37A552174aFe724D8180c25";
+ * const paymasterParams = utils.getPaymasterParams(paymasterAddress, {
+ *   type: "General",
+ *   innerInput: new Uint8Array(),
+ * });
+ *
+ * @example create approval-based parameters
+ *
+ * const result = utils.getPaymasterParams("0x0a67078A35745947A37A552174aFe724D8180c25", {
+ *   type: "ApprovalBased",
+ *   token: "0x65C899B5fb8Eb9ae4da51D67E1fc417c7CB7e964",
+ *   minimalAllowance: BigInt(1),
+ *   innerInput: new Uint8Array(),
+ * });
+ */
 export function getPaymasterParams(
   paymasterAddress: Address,
   paymasterInput: PaymasterInput
