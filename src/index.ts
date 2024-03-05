84008bbe955e21562df38dd2d254d5cd5ed3713d
danijelTxFusion <danijel@txfusion.io>
2024-03-05 16:19:17 +0100

    feat(signer): add `L2VoidSigner` and `L1VoidSigner`

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
 export * as utils from './utils';
 export * as types from './types';
-export {EIP712Signer, Signer, L1Signer} from './signer';
+export {
+  EIP712Signer,
+  Signer,
+  L1Signer,
+  L2VoidSigner,
+  L1VoidSigner,
+} from './signer';
 export {Wallet} from './wallet';
 export {BrowserProvider, Provider} from './provider';
 export {ContractFactory, Contract} from './contract';
