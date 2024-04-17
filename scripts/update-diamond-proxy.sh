549b72d471bc2b6c4358df73fe5d3f46806e4d5d
mm <mm@matterlabs.dev>
2024-04-17 16:47:49 +0200

    fix: small fixes to make it work with new local node

--- scripts/update-diamond-proxy.sh
+++ scripts/update-diamond-proxy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-output=$(docker exec local-setup_zksync_1 cat /deployed_contracts.log)
+output=$(docker exec local-setup-zksync-1 cat /deployed_contracts.log)
 address=$(echo "$output" | grep CONTRACTS_DIAMOND_PROXY_ADDR | cut -d'=' -f2 | tr -d '[:space:]')
 echo "{\"address\": \"$address\"}" > ../tests/files/diamondProxy.json
