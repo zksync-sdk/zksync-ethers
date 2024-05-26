2c0f17060b006a512ce3e8a573ed6e6fecb81521
danijelTxFusion <danijel@txfusion.io>
2024-05-26 14:39:59 +0200

    build: remove diamong proxy setup

--- scripts/update-diamond-proxy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-output=$(docker exec local-setup_zksync_1 cat /deployed_contracts.log)
-address=$(echo "$output" | grep CONTRACTS_DIAMOND_PROXY_ADDR | cut -d'=' -f2 | tr -d '[:space:]')
-echo "{\"address\": \"$address\"}" > ../tests/files/diamondProxy.json
