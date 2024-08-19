1ffd39aeda718746142231c9775964f5c0148216
petarTxFusion <petar@txfusion.io>
2024-08-19 16:04:34 +0200

    feat(provider): add `estimateDefaultBridgeDepositL2Gas` and `estimateCustomBridgeDepositL2Gas`

--- tests/integration/provider.test.ts
+++ tests/integration/provider.test.ts
@@ -10,13 +10,14 @@ import {
   DAI_L1,
   APPROVAL_TOKEN,
   PAYMASTER,
-  L2_CHAIN_URL,
-} from '../utils';
+  L2_CHAIN_URL, L1_CHAIN_URL
+} from "../utils";
 import { EIP712_TX_TYPE } from "../../src/utils";
 
 describe('Provider', () => {
   const provider = new Provider(L2_CHAIN_URL);
   const wallet = new Wallet(PRIVATE_KEY1, provider);
+  const ethProvider = ethers.getDefaultProvider(L1_CHAIN_URL);
 
   let receipt: types.TransactionReceipt;
   let baseToken: string;
@@ -914,6 +915,65 @@ describe('Provider', () => {
       });
       expect(result > 0n).to.be.true;
     });
+  });
+
+  describe('#estimateDefaultBridgeDepositL2Gas()', () => {
+    if (IS_ETH_BASED) {
+      it('should return estimation for ETH token', async () => {
+        const result = await provider.estimateDefaultBridgeDepositL2Gas(
+          ethProvider,
+          utils.LEGACY_ETH_ADDRESS,
+          ethers.parseEther('1'),
+          ADDRESS2,
+          ADDRESS1
+        );
+        expect(result > 0n).to.be.true;
+      });
+
+      it('should return estimation for DAI token', async () => {
+        const result = await provider.estimateDefaultBridgeDepositL2Gas(
+          ethProvider,
+          DAI_L1,
+          5,
+          ADDRESS2,
+          ADDRESS1
+        );
+        expect(result > 0n).to.be.true;
+      });
+    } else {
+      it('should return estimation for ETH token', async () => {
+        const result = await provider.estimateDefaultBridgeDepositL2Gas(
+          ethProvider,
+          utils.LEGACY_ETH_ADDRESS,
+          ethers.parseEther('1'),
+          ADDRESS2,
+          ADDRESS1
+        );
+        expect(result > 0n).to.be.true;
+      });
+
+      it('should return estimation for base token', async () => {
+        const result = await provider.estimateDefaultBridgeDepositL2Gas(
+          ethProvider,
+          await provider.getBaseTokenContractAddress(),
+          ethers.parseEther('1'),
+          ADDRESS2,
+          ADDRESS1
+        );
+        expect(result > 0n).to.be.true;
+      });
+
+      it('should return estimation for DAI token', async () => {
+        const result = await provider.estimateDefaultBridgeDepositL2Gas(
+          ethProvider,
+          DAI_L1,
+          5,
+          ADDRESS2,
+          ADDRESS1
+        );
+        expect(result > 0n).to.be.true;
+      });
+    }
   });
 
   describe('#getFilterChanges()', () => {
