559896b659a280ba303a9ee0f33feab519191bcd
petarTxFusion <petar@txfusion.io>
2024-09-26 17:07:49 +0200

    feat: utilize estimated `gasPerPubData`

--- tests/utils.ts
+++ tests/utils.ts
@@ -9,9 +9,9 @@ export const PRIVATE_KEY2 =
 export const DAI_L1 = '0x70a0F165d6f8054d0d0CF8dFd4DD2005f0AF6B55';
 export const APPROVAL_TOKEN = '0x841c43Fa5d8fFfdB9efE3358906f7578d8700Dd4'; // Crown token
 export const PAYMASTER = '0xa222f0c183AFA73a8Bc1AFb48D34C88c9Bf7A174'; // Crown token paymaster
-
-export const IS_ETH_BASED = false;
-export const L2_CHAIN_URL = IS_ETH_BASED
+export const IS_ETH_BASED = ['true', '1', 'yes'].includes(
+  process.env.IS_ETH_CHAIN ?? 'true'
+);export const L2_CHAIN_URL = IS_ETH_BASED
   ? 'http://127.0.0.1:15100'
   : 'http://127.0.0.1:15200';
 export const L1_CHAIN_URL = 'http://127.0.0.1:15045';
