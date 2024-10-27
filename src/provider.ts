a658267eb92d3ecbf7f6c0ffb81b6756838b08e1
petarTxFusion <petar@txfusion.io>
2024-10-27 11:16:43 +0100

    feat(provider): add support for provider legacy fetcher

--- src/provider.ts
+++ src/provider.ts
@@ -61,6 +61,8 @@ import Formatter = providers.Formatter;
 import {Il2SharedBridgeFactory} from './typechain/Il2SharedBridgeFactory';
 import {Il2Bridge} from './typechain/Il2Bridge';
 import {Il2SharedBridge} from './typechain/Il2SharedBridge';
+import {Networkish} from '@ethersproject/networks';
+import {deepCopy} from '@ethersproject/properties';
 
 let defaultFormatter: Formatter | null = null;
 
@@ -2113,6 +2115,125 @@ export class Provider extends ethers.providers.JsonRpcProvider {
   }
 }
 
+let _nextId = 1;
+
+type Web3LegacySend = (
+  request: any,
+  callback: (error: Error, response: any) => void
+) => void;
+
+export type JsonRpcFetchFunc = (
+  method: string,
+  params?: Array<any>
+) => Promise<any>;
+
+function buildWeb3LegacyFetcher(
+  provider: ExternalProvider,
+  sendFunc: Web3LegacySend
+): JsonRpcFetchFunc {
+  const fetcher = 'Web3LegacyFetcher';
+
+  // @ts-ignore
+  return function (method: string, params: Array<any>): Promise<any> {
+    const request = {
+      method: method,
+      params: params,
+      id: _nextId++,
+      jsonrpc: '2.0',
+    };
+
+    return new Promise((resolve, reject) => {
+      // @ts-ignore
+      this.emit('debug', {
+        action: 'request',
+        fetcher,
+        request: deepCopy(request),
+        // @ts-ignore
+        provider: this,
+      });
+
+      sendFunc(request, (error, response) => {
+        if (error) {
+          // @ts-ignore
+          this.emit('debug', {
+            action: 'response',
+            fetcher,
+            error,
+            request,
+            // @ts-ignore
+            provider: this,
+          });
+
+          return reject(error);
+        }
+        // @ts-ignore
+        this.emit('debug', {
+          action: 'response',
+          fetcher,
+          request,
+          response,
+          // @ts-ignore
+          provider: this,
+        });
+
+        if (response.error) {
+          const error = new Error(response.error.message);
+          (<any>error).code = response.error.code;
+          (<any>error).data = response.error.data;
+          return reject(error);
+        }
+
+        resolve(response.result);
+      });
+    });
+  };
+}
+
+function buildEip1193Fetcher(provider: ExternalProvider): JsonRpcFetchFunc {
+  // @ts-ignore
+  return function (method: string, params: Array<any>): Promise<any> {
+    if (params === null || params === undefined) {
+      params = [];
+    }
+
+    const request = {method, params};
+    // @ts-ignore
+    this.emit('debug', {
+      action: 'request',
+      fetcher: 'Eip1193Fetcher',
+      request: deepCopy(request),
+      provider: provider,
+    });
+    // @ts-ignore
+    return provider.request(request).then(
+      response => {
+        // @ts-ignore
+        this.emit('debug', {
+          action: 'response',
+          fetcher: 'Eip1193Fetcher',
+          request,
+          response,
+          provider: provider,
+        });
+
+        return response;
+      },
+      error => {
+        // @ts-ignore
+        this.emit('debug', {
+          action: 'response',
+          fetcher: 'Eip1193Fetcher',
+          request,
+          error,
+          provider: provider,
+        });
+
+        throw error;
+      }
+    );
+  };
+}
+
 /* c8 ignore start */
 /**
  * A `Web3Provider` extends {@link ExternalProvider} and includes additional features for interacting with ZKsync Era.
@@ -2122,6 +2243,7 @@ export class Provider extends ethers.providers.JsonRpcProvider {
  */
 export class Web3Provider extends Provider {
   readonly provider: ExternalProvider;
+  readonly jsonRpcFetchFunc: JsonRpcFetchFunc;
 
   /**
    * Connects to the `ethereum` provider, optionally forcing the `network`.
@@ -2136,22 +2258,56 @@ export class Web3Provider extends Provider {
    * const provider = new Web3Provider(window.ethereum);
    */
   constructor(
-    provider: ExternalProvider,
-    network?: ethers.providers.Networkish
+    provider: ExternalProvider | JsonRpcFetchFunc,
+    network?: Networkish
   ) {
-    if (!provider) {
+    if (provider === null) {
       throw new Error('Missing provider!');
     }
-    if (!provider.request) {
-      throw new Error('Provider must implement eip-1193!');
+
+    let path: string | null = null;
+    let jsonRpcFetchFunc: JsonRpcFetchFunc | null = null;
+    let subprovider: ExternalProvider | null = null;
+
+    if (typeof provider === 'function') {
+      path = 'unknown:';
+      jsonRpcFetchFunc = provider;
+    } else {
+      path = provider.host || provider.path || '';
+      if (!path && provider.isMetaMask) {
+        path = 'metamask';
+      }
+
+      subprovider = provider;
+
+      if (provider.request) {
+        if (path === '') {
+          path = 'eip-1193:';
+        }
+        jsonRpcFetchFunc = buildEip1193Fetcher(provider);
+      } else if (provider.sendAsync) {
+        jsonRpcFetchFunc = buildWeb3LegacyFetcher(
+          provider,
+          provider.sendAsync.bind(provider)
+        );
+      } else if (provider.send) {
+        jsonRpcFetchFunc = buildWeb3LegacyFetcher(
+          provider,
+          provider.send.bind(provider)
+        );
+      } else {
+        throw new Error('unsupported provider');
+      }
+
+      if (!path) {
+        path = 'unknown:';
+      }
     }
 
-    const path =
-      provider.host ||
-      provider.path ||
-      (provider.isMetaMask ? 'metamask' : 'eip-1193:');
     super(path, network);
-    this.provider = provider;
+
+    this.jsonRpcFetchFunc = jsonRpcFetchFunc!;
+    this.provider = subprovider!;
   }
 
   /**
@@ -3095,7 +3251,7 @@ export class Web3Provider extends Provider {
       method = 'personal_sign';
       params = [params[1], params[0]];
     }
-    return await this.provider.request!({method, params});
+    return await this.jsonRpcFetchFunc(method, params);
   }
 
   /**
