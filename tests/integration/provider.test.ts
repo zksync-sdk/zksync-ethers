55803e8963256f5270f44d2ac9163af1b6c86654
kelemeno <34402761+kelemeno@users.noreply.github.com>
2025-03-27 08:38:21 +0000

    feat: support v26, remove support for v25 (#243)
    * changes
    
    * (feat): update SDK to perform L2 to L1 bridging & test
    
    * (fix): revert LEGACY ETH address for tests, extend tests for L2-L1 bridging
    
    * (fix): push build
    
    * (fix): add typechain to build
    
    * provider assetId fix
    
    * fix
    
    * use new encoding
    
    * continue upd address
    
    * new encoding in sdk
    
    * fixes from mp
    
    * fix
    
    * build
    
    * fix sdk
    
    * new approach for withdrawal
    
    * fix: tests
    
    * fix: lint
    
    * build
    
    * fix: allow null for `totalDifficulty`
    
    * 6.15.3 version bump [skip ci]
    
    ## [6.15.3](https://github.com/zksync-sdk/zksync-ethers/compare/v6.15.2...v6.15.3) (2024-12-06)
    
    ### Bug Fixes
    
    * allow null for `totalDifficulty` ([4681d5b](https://github.com/zksync-sdk/zksync-ethers/commit/4681d5b887f652e1f7d050b4c40f80763169e110))
    
    * docs: fix errors
    
    * fix: `populateTransaction` estimate fee only if necessary
    
    * 6.15.4 version bump [skip ci]
    
    ## [6.15.4](https://github.com/zksync-sdk/zksync-ethers/compare/v6.15.3...v6.15.4) (2025-01-17)
    
    ### Bug Fixes
    
    * `populateTransaction` estimate fee only if necessary ([8c85348](https://github.com/zksync-sdk/zksync-ethers/commit/8c85348fe4a323f1d37c350d8d9963ffe1de1788))
    
    * test: updated local-setup with gateway
    
    * fix: add custom branch for non-eth-based test
    
    * fix: provide dynamic DAI address
    
    * fix: build
    
    * fix: tests
    
    * fix: prettier
    
    * fix: move function to utils
    
    * test: pushing build folders
    
    * fix: build files
    
    * increase L1_RECOMMENDED_MIN_ERC20_DEPOSIT_GAS_LIMIT
    
    * esm typechain
    
    * add method for artur
    
    * add back removed v25 features
    
    * add old setup testing
    
    * add DAI_V25 and 26
    
    * some build issues
    
    * small fixes
    
    * attempt more fixes
    
    * gas estimation for previous version
    
    * remove build
    
    * fix test attempt
    
    * wrong order
    
    * linting
    
    * try commenting out spi update
    
    * Revert "remove build"
    
    This reverts commit 0671af31a072e13731f01764911ea2ba4052877e.
    
    * fix: run on high-performance machine
    
    * fix: run on high-performance machine
    
    * fix: run on high-performance machine
    
    * fix: run on high-performance machine
    
    * revert: use ubuntu-latest runners as before
    
    * finalizeDepositParamStruct
    
    * update local setup branches
    
    * fix: gas estimate
    
    * test: if increasing timeout would make CI green
    
    * checkout legacy branch for v25 non-eth
    
    * branch changes in CI
    
    * protocol version erros debugging
    
    * remove setup checkout
    
    * we're already on v27 in the local setup
    
    * v26 more === to >=
    
    * v24 in previous local setup
    
    * build
    
    * fix: comparison of protocol version if claim failed deposit
    
    * timeout
    
    * another timeout increase
    
    * remove build
    
    * push intermediate state between upgrade
    
    * increase timeout 3
    
    * some changes
    
    * ci
    
    * some more cleanup
    
    * extra branch
    
    * some more cleanup
    
    * some linting
    
    * small fixes
    
    * lint
    
    * further fixes
    
    * caching address issue
    
    * issues
    
    * last refactoring
    
    * rename protovolisnew
    
    * error catching
    
    * remove v25 support
    
    * remove more unused code
    
    * more unused code
    
    * remove bad file
    
    * round 2 of issues
    
    * more fixes
    
    * more comments
    
    * linting
    
    * fixes
    
    * comment
    
    * lint
    
    * comment
    
    ---------
    
    Co-authored-by: Raid Ateir <ra@matterlabs.dev>
    Co-authored-by: Stanislav Breadless <stanislavbezkor@gmail.com>
    Co-authored-by: petarTxFusion <petar@txfusion.io>
    Co-authored-by: perekopskiy <mikeson.dp@gmail.com>
    Co-authored-by: semantic-release-bot <semantic-release-bot@martynus.net>
    Co-authored-by: danijelTxFusion <danijel@txfusion.io>
    Co-authored-by: Raid5594 <52794079+Raid5594@users.noreply.github.com>

--- tests/integration/provider.test.ts
+++ tests/integration/provider.test.ts
@@ -14,12 +14,10 @@ import {
   L1_CHAIN_URL,
 } from '../utils';
 import {EIP712_TX_TYPE} from '../../src/utils';
-
 describe('Provider', () => {
   const provider = new Provider(L2_CHAIN_URL);
   const wallet = new Wallet(PRIVATE_KEY1, provider);
   const ethProvider = ethers.getDefaultProvider(L1_CHAIN_URL);
-
   let receipt: types.TransactionReceipt;
   let baseToken: string;
 
@@ -320,7 +318,7 @@ describe('Provider', () => {
       const result = await provider.l1TokenAddress(
         await provider.l2TokenAddress(DAI_L1)
       );
-      expect(result).to.be.equal(DAI_L1);
+      expect(result.toLowerCase()).to.equal(DAI_L1.toLowerCase());
     });
   });
 
@@ -418,9 +416,9 @@ describe('Provider', () => {
         const tx = {
           from: ADDRESS1,
           value: 7_000_000_000n,
+          type: 113,
           to: utils.L2_BASE_TOKEN_ADDRESS,
           data: '0x51cff8d900000000000000000000000036615cf349d7f6344891b1e7ca7c72883f5dc049',
-          type: 113,
           customData: {
             paymasterParams: {
               paymaster: '0x0EEc6f45108B4b806e27B81d9002e162BD910670',
@@ -540,6 +538,7 @@ describe('Provider', () => {
     it('should return a DAI withdraw transaction', async () => {
       const tx = {
         type: 113,
+        value: 5n,
         from: ADDRESS1,
         to: (await provider.getDefaultBridgeAddresses()).sharedL2,
         data: '0xd9caed1200000000000000000000000036615cf349d7f6344891b1e7ca7c72883f5dc04900000000000000000000000082b5ea13260346f4251c0940067a9117a6cf13840000000000000000000000000000000000000000000000000000000000000005',
@@ -556,6 +555,7 @@ describe('Provider', () => {
     it('should return a DAI withdraw transaction with paymaster parameters', async () => {
       const tx = {
         type: 113,
+        value: 5n,
         from: ADDRESS1,
         to: (await provider.getDefaultBridgeAddresses()).sharedL2,
         data: '0xd9caed1200000000000000000000000036615cf349d7f6344891b1e7ca7c72883f5dc04900000000000000000000000082b5ea13260346f4251c0940067a9117a6cf13840000000000000000000000000000000000000000000000000000000000000005',
@@ -578,6 +578,22 @@ describe('Provider', () => {
           minimalAllowance: 1,
           innerInput: new Uint8Array(),
         }),
+      });
+      expect(result).to.be.deepEqualExcluding(tx, ['data']);
+    });
+
+    it('should return a Crown withdraw transaction', async () => {
+      const tx = {
+        type: 113,
+        from: ADDRESS1,
+        to: (await provider.getDefaultBridgeAddresses()).sharedL2,
+        data: '0xd9caed1200000000000000000000000036615cf349d7f6344891b1e7ca7c72883f5dc04900000000000000000000000082b5ea13260346f4251c0940067a9117a6cf13840000000000000000000000000000000000000000000000000000000000000005',
+      };
+      const result = await provider.getWithdrawTx({
+        token: APPROVAL_TOKEN,
+        amount: 5,
+        to: ADDRESS1,
+        from: ADDRESS1,
       });
       expect(result).to.be.deepEqualExcluding(tx, ['data']);
     });
@@ -1042,11 +1058,9 @@ describe('Provider', () => {
           from: ADDRESS1,
         });
       } catch (e) {
-        expect(
-          (e as Error).message
-            .toString()
-            .includes('insufficient balance for transfer')
-        ).to.be.true;
+        const revertString = 'insufficient funds for gas + value.';
+        expect((e as Error).message.toString().includes(revertString)).to.be
+          .true;
       }
     });
   });
